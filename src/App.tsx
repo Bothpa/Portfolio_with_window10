7ae821611c51ad4966cb776b6b60a540d488b65d
jungsonghun <h1jshmal1@naver.com>
2024-09-06 06:32:36 +0000

    너무 완벽하고 ㅋㅋ

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,46 @@
-import TaskBar from "./Components/TaskBar/TaskBar";
+import { useEffect, useState } from "react";
+import TaskBar from "./Components/TaskBar";
+import Booting from "./Components/Booting";
 
 function App() {
+  const [isFullScreen, setFullScreen] = useState(false)
+  const [isBoot, setBoot] = useState(false)
+
+  const toggleFullScreen = () => {
+    setTimeout(() => {
+      if (document.fullscreenElement) {
+        document.exitFullscreen()
+      }  else {
+       document.documentElement.requestFullscreen()
+      }
+      setBoot(true)
+      setTimeout(() => {
+        setBoot(false)
+        setFullScreen(true)
+      }, 4000)
+    }, 500)
+  }
+
+
   return (
-    <div className="w-screen h-screen wdbi flex">
-      <TaskBar />
-    </div>
+    <>
+      {isFullScreen ? (
+        <div className="w-screen h-screen wdbi flex">
+          <TaskBar />
+        </div>
+      ) : (
+        <div className="w-screen h-screen flex flex-col items-center justify-center text-3xl font-bold">
+          <img src="/Images/Switch.png" className="btn-3d color" alt="전원버튼" onMouseUp={toggleFullScreen} />
+          <Booting isBoot={isBoot}/>
+        </div>
+      )
+      }
+    </>
   );
 }
 
 export default App;
+
+function sleep(arg0: number) {
+  throw new Error("Function not implemented.");
+}
--- src/Components/TaskBar.tsx
+++ src/Components/TaskBar.tsx
@@ -1,7 +1,7 @@
 const TaskBar = () => {
     return (
         <div className="w-full h-[39px] mt-auto bg-[#1C1C1C]">
-
+            
         </div>
     )
 };
